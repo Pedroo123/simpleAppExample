ebb7cab179026a5cc924f39ea8d348a391d278a0
Pedro brancalh�o <pedroaugusto466@gmail.com>
2019-06-12 23:09:44 -0300

    First commit

---
+++ utils/WeatherApiKey.js
@@ -0,0 +1,1 @@
+export const API_KEY = 'b4970b6eaf5186efb14ada213b799688';
---
+++ utils/WeatherConditions.js
@@ -0,0 +1,51 @@
+export const weatherConditions = {
+    Rain: {
+      color: '#005BEA',
+      title: 'Tá chovendo nego',
+      subtitle: 'Bora assistir netflix',
+      icon: 'weather-rainy'
+    },
+    Clear: {
+      color: '#f7b733',
+      title: 'Sol pa karai',
+      subtitle: 'Passa protetor meu bom',
+      icon: 'weather-sunny'
+    },
+    Thunderstorm: {
+      color: '#616161',
+      title: 'Vai chove pra pora',
+      subtitle: 'É bom levar guarda-chuva',
+      icon: 'weather-lightning'
+    },
+    Clouds: {
+      color: '#1F1C2C',
+      title: 'Da pra ver nada',
+      subtitle: 'Tudo nuvem',
+      icon: 'weather-cloudy'
+    },
+
+    Snow: {
+      color: '#00d2ff',
+      title: 'Neve',
+      subtitle: 'aqui não tem neve',
+      icon: 'weather-snowy'
+    },
+    Drizzle: {
+      color: '#076585',
+      title: 'Garoando',
+      subtitle: 'Ta parecendo São-Paulo',
+      icon: 'weather-hail'
+    },
+    Haze: {
+      color: '#66A6FF',
+      title: 'Neblina',
+      subtitle: 'É garoa só que mais fraca',
+      icon: 'weather-hail'
+    },
+    Mist: {
+      color: '#3CD3AD',
+      title: 'Neblina pesada',
+      subtitle: "Cuidado meu considerado",
+      icon: 'weather-fog'
+    }
+  };
